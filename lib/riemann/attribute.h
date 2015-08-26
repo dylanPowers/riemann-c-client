02cb22b828a08a0f997e0997fab23790b608db23
Gergely Nagy <algernon@madhouse-project.org>
2015-08-26 13:17:16 +0200

    Remove doxygen-targetting comments and doxygen support
    With the new prosaic docs, drop doxygen. Makes the headers smaller,
    easier to navigate, and the docs become more approachable.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- lib/riemann/attribute.h
+++ lib/riemann/attribute.h
@@ -15,131 +15,30 @@
  * License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file lib/riemann/attribute.h
- */
-
 #ifndef __MADHOUSE_RIEMANN_ATTRIBUTE_H__
 #define __MADHOUSE_RIEMANN_ATTRIBUTE_H__ 1
 
 #include <riemann/proto/riemann.pb-c.h>
 
-/** @defgroup riemann_attribute Riemann Attributes
- *
- * Attributes can be attached to events, they are user-specified
- * key-value pairs.
- *
- * @see riemann_event
- *
- * @addtogroup riemann_attribute
- * @{
- */
-
-/** An attribute.
- *
- * Attributes are key-value pairs, they have `key` and `value`
- * members.
- */
 typedef Attribute riemann_attribute_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** Allocate a new attribute.
- *
- * Allocates a new - empty - attribute, with neither key nor value
- * set. The returned object must be freed with
- * riemann_attribute_free().
- *
- * @returns A newly allocated attribute object.
- */
 riemann_attribute_t *riemann_attribute_new (void);
-
-/** Clone an existing attribute.
- *
- * @param attrib is the existing attribute to copy.
- *
- * The returned copy must be freed with riemann_attribute_free(). The
- * original will remain untouched.
- *
- * @retval NULL is returned on error, and `errno` will be set to
- * `EINVAL` if the supplied attribute was NULL.
- * @retval riemann_attribute_t object - a copy of the supplied
- * attribute - is returned otherwise.
- */
 riemann_attribute_t *riemann_attribute_clone (const riemann_attribute_t *attrib);
-
-/** Create a new attribute with a key-value pair.
- *
- * @param key is the optional key part of the attribute.
- * @param value is the optional value part of the attribute.
- *
- * With both arguments set to NULL, this is equivalent to
- * riemann_attribute_new(). Similarly, the returned object must be
- * freed with riemann_attribute_free().
- *
- * Both `key` and `value` will be copied, ownership remains at the
- * caller.
- *
- * @returns A newly allocated object with key and value already set.
- */
 riemann_attribute_t *riemann_attribute_create (const char *key,
                                                const char *value);
-/** Free an attribute.
- *
- * @param attrib is the attribute to free.
- *
- * @note Sets `errno` to `EINVAL` in case the supplied attribute was
- * NULL.
- */
 void riemann_attribute_free (riemann_attribute_t *attrib);
 
-/** Set the key part of an attribute.
- *
- * @param attrib is the attribute to set the key part of.
- * @param key is the key to use.
- *
- * The key will be copied internally, ownership remains at the caller.
- *
- * @retval 0 is returned on success.
- * @retval -errno is returned on failure.
- */
 int riemann_attribute_set_key (riemann_attribute_t *attrib, const char *key);
-
-/** Set the value part of an attribute.
- *
- * @param attrib is the attribute to set the key part of.
- * @param value is the value to use.
- *
- * The value will be copied internally, ownership remains at the caller.
- *
- * @retval 0 is returned on success.
- * @retval -errno is returned on failure.
- */
 int riemann_attribute_set_value (riemann_attribute_t *attrib, const char *value);
-
-/** Set the both key and value parts of an attribute.
- *
- * @param attrib is the attribute to set the key and value of.
- * @param key is the key to use.
- * @param value is the value to use.
- *
- * Both `key` and `value` will be copied, ownership remains at the
- * caller. If either of the arguments is NULL, the function will
- * fail. However, setting both properties is not atomic: if the
- * function fails, one of the properties may have been changed
- * already. In this case, it is best to discard the object altogether.
- *
- * @retval 0 is returned on success.
- * @retval -errno is returned on failure.
- */
 int riemann_attribute_set (riemann_attribute_t *attrib,
                            const char *key, const char *value);
 
 #ifdef __cplusplus
-} /* extern "C" */
+}
 #endif
-
-/** @} */
 
 #endif
--- lib/riemann/simple.h
+++ lib/riemann/simple.h
@@ -1,5 +1,5 @@
 /* riemann/simple.h -- Riemann C client library
- * Copyright (C) 2013  Gergely Nagy <algernon@madhouse-project.org>
+ * Copyright (C) 2013, 2015  Gergely Nagy <algernon@madhouse-project.org>
  *
  * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -15,18 +15,6 @@
  * License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file lib/riemann/simple.h
- */
-
-/** @defgroup riemann_simple Simple helpers for one-off messages
- *
- * These are various helper functions to send one-off messages to a
- * Riemann server.
- *
- * @addtogroup riemann_simple
- * @{
- */
-
 #ifndef __MADHOUSE_RIEMANN_SIMPLE_H__
 #define __MADHOUSE_RIEMANN_SIMPLE_H__ 1
 
@@ -36,111 +24,23 @@
 extern "C" {
 #endif
 
-/** Send a one-off event-filled message.
- *
- * @param client is the client to send with.
- * @param field is the first field.
- * @param ... are the field arguments, and further field descriptions,
- * terminated by #RIEMANN_EVENT_FIELD_NONE.
- *
- * Constructs a new message like if
- * riemann_message_create_with_events() was called with the same
- * arguments, sends that through the supplied client, and then frees
- * up the message.
- *
- * Intended for one-off messages.
- *
- * @retval 0 is returned on success.
- * @retval -errno is returned on failure.
- */
 int riemann_send (riemann_client_t *client,
                   riemann_event_field_t field, ...);
-
-/** Send a one-off event-filled message.
- *
- * @param client is the client to send with.
- * @param field is the first field.
- * @param aq the field arguments, and further field descriptions,
- * terminated by #RIEMANN_EVENT_FIELD_NONE, wrapped in a `va_list`.
- *
- * Constructs a new message like if
- * riemann_message_create_with_events_va() was called with the same
- * arguments, sends that through the supplied client, and then frees
- * up the message.
- *
- * Intended for one-off messages.
- *
- * @retval 0 is returned on success.
- * @retval -errno is returned on failure.
- */
 int riemann_send_va (riemann_client_t *client,
                      riemann_event_field_t field, va_list aq);
 
-/** Send a one-off query.
- *
- * @param client is the client to query with.
- * @param query is the query string to use.
- *
- * Constructs a new query object, as if riemann_query_new() was called
- * with the same argument, sends the query, and waits for a reply. The
- * reply is parsed and returned, and the query freed.
- *
- * @retval riemann_message_t object on success, the result of the
- * query. The object must be freed with riemann_message_free() once no
- * longer needed.
- * @retval NULL is returned upon failure, and `errno` is set
- * appropriately in this case.
- */
 riemann_message_t *riemann_query (riemann_client_t *client,
                                   const char *query);
 
-/** Send & receive a message.
- *
- * Sends a message, and if need be, waits for a reply.
- *
- * @param client is the client to send and receive with.
- * @param message is the message to send. The object will be freed
- * before the function returns.
- *
- * @returns The newly allocated response message (query results, ACK
- * over TCP and TLS, generated response on UDP), or NULL on
- * communication error.
- *
- */
 riemann_message_t *riemann_communicate (riemann_client_t *client,
                                         riemann_message_t *message);
-
-/** Send a query, and receive replies.
- *
- * Sends a query, and collects the replies.
- *
- * @param client is the client to send and receive with.
- * @param query_string is the query in string format.
- *
- * @returns The newly allocated response message, with results
- * included, or NULL on communication error.
- */
 riemann_message_t *riemann_communicate_query (riemann_client_t *client,
                                               const char *query_string);
-
-/** Send an event, and receive the ACK (if any).
- *
- * Sends a single event, and waits for the ACK (or fakes one, when
- * used over UDP).
- *
- * @param client is the client to send and receive with.
- * @param field is the first event field.
- * @param ... are the value and the rest of the field-value pairs.
- *
- * @returns The newly allocated ACK message.
- */
 riemann_message_t *riemann_communicate_event (riemann_client_t *client,
                                               riemann_event_field_t field, ...);
 
 #ifdef __cplusplus
-} /* extern "C" */
+}
 #endif
-
-/** @} */
 
 #endif
